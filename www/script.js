15ae67ab891cd59adabfdddd90811c99896ee873
ratho3009 <143334934+ratho3009@users.noreply.github.com>
2025-07-30 16:40:31 +1000

    Update script.js

---
+++ www/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('#loginForm').on('submit', function (e) {
+      e.preventDefault();
+  
+      const email = $('input[name="email"]').val();
+      const password = $('input[name="password"]').val();
+  
+      $.post('/login', { email, password }, function (response) {
+        if (response.valid) {
+          // go to account page
+          window.location.href = '/account';
+        } else {
+          // show error message
+          $('#errormsg').removeClass('d-none').addClass('d-block');
+        }
+      });
+    });
+  });
